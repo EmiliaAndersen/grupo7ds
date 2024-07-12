538ee838f3ed12c6234a6cf4163dcff8dafc0095
Ariel-Hab <ari.habib.arg@gmail.com>
2024-07-12 11:50:47 -0300

    version para la presentación

--- Entrega_2/src/test/java/MigracionTest.java
+++ Entrega_2/src/test/java/MigracionTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -55,40 +54,6 @@ public class MigracionTest {
         }
     }
 
-    @Test
-    public void migracion_archivo_csv() {
-        // --- EJEMPLO MIGRACION ---
-        Migrador reg = new Migrador();
-        List<PersonaHumana> personas = new ArrayList<>();
-        List<Colaborador> colaboradores = new ArrayList<>();
-        reg.Migrar("../Entrega_2/src/main/java/org/example/archivo.csv", personas, colaboradores);
-        // Chequeo que haya salido todo bien
 
-        List<String> mailsPrueba = Arrays.asList("juan@example.com", "maria@example.com", "pedro@example.com");
-
-
-        System.out.println(colaboradores.size());
-
-        int i = 0;
-        for (Colaborador colaborador : colaboradores) {
-            Persona persona = colaborador.getPersona();
-            if (persona != null) {
-                List<MedioDeContacto> medios = persona.getMediosDeContacto();
-                MedioDeContacto contacto = null;
-                if (medios != null && !medios.isEmpty()) {
-                    contacto = medios.get(0);
-                    System.out.println(contacto.getDetalle());
-                } else {
-                    System.out.println("La persona no tiene medios de contacto registrados.");
-                }
-                System.out.println(colaborador.getColaboraciones());
-                assert contacto != null;
-                Assertions.assertEquals(contacto.getDetalle(), mailsPrueba.get(i));
-            } else {
-                System.out.println("El colaborador no tiene persona asignada.");
-            }
-            i++;
-        }
-    }
 
 }
