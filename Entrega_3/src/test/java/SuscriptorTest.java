ed9acfa35c1c342515092e5921403cce902fa6cf
Emilia Andersen <129566261+EmiliaAndersen@users.noreply.github.com>
2024-09-12 17:42:13 -0300

    Create SuscriptorTest.java

---
+++ Entrega_3/src/test/java/SuscriptorTest.java
@@ -0,0 +1,31 @@
+import org.example.Dominio.Heladeras.Heladera;
+import org.example.Dominio.PuntosEstrategicos.PuntoEstrategico;
+import org.example.Dominio.Rol.Colaborador;
+import org.example.Dominio.Viandas.Vianda;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.example.Dominio.Suscripciones.TipoSuscripcion.RESTANTES;
+
+class SuscriptorTest {
+
+    @Test
+    void testSuscripcion() {
+        Colaborador colaborador = new Colaborador();
+        PuntoEstrategico punto = new PuntoEstrategico("zona1", 10.0, 120.0, "Av Diaz 12");
+        Heladera heladera = new Heladera(10, 0, punto);
+
+        colaborador.suscribirme(RESTANTES, heladera, 2);
+
+        Vianda vianda1 = new Vianda(null, null, null, null, 0, 0, null);
+        Vianda vianda2 = new Vianda(null, null, null, null, 0, 0, null);
+        heladera.agregarVianda(vianda1);
+        heladera.agregarVianda(vianda2);
+
+        //Notifico
+        heladera.suscriptores.get(0).notificarSuscriptor();
+
+
+        Assertions.assertEquals(heladera.suscriptores.get(0).mensajesEnviados.size(),1);
+    }
+}
