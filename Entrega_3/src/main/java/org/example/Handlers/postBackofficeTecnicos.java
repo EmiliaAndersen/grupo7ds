c550880ac1c95c79e573c9f697904b47f6e1dba0
DanJudzik7 <djudzik@frba.utn.edu.ar>
2024-12-09 19:00:12 -0300

    Update names del front

--- Entrega_3/src/main/java/org/example/Handlers/postBackofficeTecnicos.java
+++ Entrega_3/src/main/java/org/example/Handlers/postBackofficeTecnicos.java
@@ -4,13 +4,27 @@ import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+
 public class postBackofficeTecnicos implements Handler {
   @Override
   public void handle(@NotNull Context context) throws Exception {
-    context.formParam("radio");
-    context.formParam("radio");
-    context.formParam("radio");
-    context.formParam("radio");
+    var model = new HashMap<String, Object>();
 
+    String nombre = context.formParam("nombre");
+    String tipoDocumento = context.formParam("tipo_documento");
+    String numeroDocumento = context.formParam("numero_documento");
+    String cuil = context.formParam("tipo_medio_contacto");
+    String contacto = context.formParam("contacto");
+    String latitud = context.formParam("latitud");
+    String longitud = context.formParam("lognitud");
+    String radio = context.formParam("radio");
+
+    try{
+      model.put("successMessage","Tecnico creado correctamente");
+    }catch (Exception e){
+      model.put("errorMessage","Error al crear Tecnico");
+    }
+    context.render("/templates/backofficeTecnicos.mustache",model);
   }
 }
